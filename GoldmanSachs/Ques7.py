41e712f55030aca4f79b5b5b4d64dd4c9bcf8872
Pratyush Gautam <pratyushgautam222@gmail.com>
2022-01-10 12:20:43 +0530

    Distributing M items in a circle of size N starting from K-th position

---
+++ GoldmanSachs/Ques7.py
@@ -0,0 +1,15 @@
+class Solution:
+    # @param A : integer
+    # @param B : integer
+    # @param C : integer
+    # @return an integer
+    def solve(self, A, B, C):
+        if A>B:
+            A = A%B
+        if C>B:
+            C = C%B
+        ans = C+A-1
+        if ans>B:
+            ans = ans%B
+
+        return ans
